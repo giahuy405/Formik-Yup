33f49aa52cfdd5f4007f3e39283cde92b1a46c08
giahuy405 <pgiahuy653@gmail.com>
2023-03-01 14:47:29 +0700

    add

--- src/App.js
+++ src/App.js
@@ -1,28 +1,9 @@
-import { createBrowserHistory } from "history";
-import { HomeTemplate } from "./template/HomeTemplates/HomeTemplates";
-import { Route, Router, Switch } from "react-router-dom";
-import Home from "./pages/Home/Home";
-import News from "./pages/News/News";
-import Contact from "./pages/Contact/Contact";
-import Login from "./pages/Login/Login";
-import Register from "./pages/Register/Register";
+
 import Demo from './Demo/Demo.jsx'
-export const history = createBrowserHistory()
+
 function App() {
   return (
-    // <Router history={history}>
-    //   <Switch>
-    //   <HomeTemplate exact path='/' Component={Home} />
-    //   <HomeTemplate exact path='/home' Component={Home} />
-    //   <HomeTemplate exact path='/news' Component={News} />
-    //   <HomeTemplate exact path='/contact' Component={Contact} />
 
-    //   <Route exact path='/contact' Component={Login} />
-    //   <Route exact path='/contact' Component={Register} />
-
-
-    //   </Switch>
-    // </Router>
     <Demo />
   );
 }
--- src/Demo/Demo.jsx
+++ src/Demo/Demo.jsx
@@ -2,8 +2,18 @@ import React from 'react';
 import { useFormik } from 'formik';
 import { basicSchema } from '../schemas';
 
+const onSubmit = async (values, actions) => {
+  
+    console.log(values);
+    console.log(actions);
+    // clear các ô input sau 1 giây nhấn submit 
+    await new Promise((resolve, reject) => setTimeout(resolve, 1000))
+    actions.resetForm();
+}
+
 const Demo = () => {
-    const { values, handleChange, handleBlur, handleSubmit } = useFormik({
+    // touched dùng để validation khi mà ta vừa gõ xong input
+    const { values, errors, handleChange, touched, handleBlur, handleSubmit ,isSubmitting } = useFormik({
         initialValues: {
             email: "",
             phone: "",
@@ -11,15 +21,15 @@ const Demo = () => {
             confirmPassword: "",
         },
         validationSchema: basicSchema,
-        
+        onSubmit,
     })
-    console.log(values);
+
     return (
         <div className='bg-gray-700 h-screen p-10'>
-            <form className="box max-w-xl mx-auto bg-amber-400 p-5 rounded-2xl shadow-2xl" onSubmit={handleSubmit}>
+            <form className="box max-w-xl mx-auto bg-amber-400 p-5 rounded-2xl shadow-2xl" onSubmit={handleSubmit} autoComplete='off'>
                 <p>Email</p>
                 <input
-                    className='w-full p-1 rounded-md'
+                    className={errors.email && touched.email ? 'w-full p-1 rounded-md border border-red-600' : 'w-full p-1 rounded-md'}
                     value={values.email}
                     onChange={handleChange}
                     onBlur={handleBlur}
@@ -27,9 +37,11 @@ const Demo = () => {
                     id='email'
                     type="text"
                 />
+                {/* hiển thị lỗi ra màn hình */}
+                {errors.email && touched.email && <span className='text-red-500  mb-3 text-xs'>{errors.email}</span>}
                 <p>Sdth</p>
                 <input
-                    className='w-full p-1 rounded-md'
+                    className={errors.phone && touched.phone ? 'w-full p-1 rounded-md border border-red-600' : 'w-full p-1 rounded-md'}
                     value={values.phone}
                     onChange={handleChange}
                     onBlur={handleBlur}
@@ -37,9 +49,11 @@ const Demo = () => {
                     id='phone'
                     type="text"
                 />
+                {/* hiển thị lỗi ra màn hình */}
+                {errors.phone && touched.phone && <span className='text-red-500  mb-3 text-xs'>{errors.phone}</span>}
                 <p>Mật khẩu</p>
                 <input
-                    className='w-full p-1 rounded-md'
+                    className={errors.password && touched.password ? 'w-full p-1 rounded-md border border-red-600' : 'w-full p-1 rounded-md'}
                     value={values.password}
                     onChange={handleChange}
                     onBlur={handleBlur}
@@ -47,9 +61,11 @@ const Demo = () => {
                     id='password'
                     type="text"
                 />
+                {/* hiển thị lỗi ra màn hình */}
+                {errors.password && touched.password && <span className='text-red-500  mb-3 text-xs'>{errors.password}</span>}
                 <p>Xác nhận mật khẩu</p>
                 <input
-                    className='w-full p-1 rounded-md'
+                    className={errors.confirmPassword && touched.confirmPassword ? 'w-full p-1 rounded-md border border-red-600' : 'w-full p-1 rounded-md'}
                     value={values.confirmPassword}
                     onChange={handleChange}
                     onBlur={handleBlur}
@@ -57,6 +73,11 @@ const Demo = () => {
                     id='confirmPassword'
                     type="text"
                 />
+                {/* hiển thị lỗi ra màn hình */}
+                {errors.confirmPassword && touched.confirmPassword && <span className='text-red-500  mb-3 text-xs'>{errors.confirmPassword}</span>}
+
+                {/* nút sẽ bị disabled mờ đi khi ta nhấn submit để tránh TH user click nhiều lần */}
+                <button disabled={isSubmitting} className='bg-black text-white w-full mt-5 hover:bg-slate-600' type='submit'>submit</button>
             </form>
         </div>
     );
